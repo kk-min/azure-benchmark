61d80e3c76709606ac9b02e23953c8d3bd09794d
kk-min <76023265+kk-min@users.noreply.github.com>
2023-10-04 14:18:39 +0800

    Update files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # snapstart-benchmark
 Program to perform manual benchmarking for AWS Lambda's SnapStart.
+
+# Usage
+1. `./scripts/setup.sh` (You may need to run `chmod +x ./scripts/setup.sh` to make it executable)
+2. `go build main.go`
+3. `./main [OPTIONS]`
+
+# Options
+- `-b`: Burst count (integer)
+- `-i`: Inter-arrival time (IAT) (milliseconds)
+- `-o`: Output directory (dir path)
--- main.go
+++ main.go
@@ -1,26 +1,102 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"flag"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strconv"
+	"sync"
 	"time"
 )
 
 var outputDir = flag.String("o", "./latency_samples/", "Output directory for latency samples (default: ./latency_samples/)")
 var burstCount = flag.Int("b", 1, "Number of bursts to send (default: 1)")
-var burstSize = flag.Int("s", 1, "Number of requests to send in a burst (default: 1)")
 var burstIAT = flag.Int("i", 10000, "Inter-arrival time between bursts in milliseconds (default: 10000)")
-var parallel = flag.Bool("p", false, "Send requests in parallel (default: false)")
 
 func main() {
 	log.Info("Starting application...")
 	flag.Parse()
-
-	outputDirPath := filepath.Join(*outputDir, time.Now().Format(time.RFC850))
-	log.Infof("Creating dir for this benchmark at `%s`", outputDirPath)
+	// Format current time in RFC3339 format
+	currentTime := time.Now().Format(time.RFC3339)
+	outputDirPath := filepath.Join(*outputDir, currentTime)
+	log.Infof("Creating output directory at %s", outputDirPath)
 	if err := os.MkdirAll(outputDirPath, os.ModePerm); err != nil {
 		log.Fatal(err)
+	}
+
+	log.Infof("Creating output files...")
+	outputFilePath := *outputDir + currentTime + "/results.csv"
+	CreateFile(outputFilePath)
+
+	endpoint := "<FUNCTION_ENDPOINT>"
+
+	var wg sync.WaitGroup
+	log.Infof("Running benchmarks...")
+	wg.Add(1)
+	go RunBenchMark(&wg, endpoint, *burstCount, outputFilePath)
+	wg.Wait()
+	log.Infof("Benchmarks completed.")
+	log.Infof("Written data to files %s and %s", *outputDir+currentTime+"/snapstart_enabled.csv", *outputDir+currentTime+"_snapstart_disabled.csv")
+}
+
+// RunCommandAndLog runs a command in the terminal, logs the result and returns it
+func RunCommandAndLog(cmd *exec.Cmd) string {
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("%s: %s", fmt.Sprint(err.Error()), stderr.String())
+	}
+	log.Debugf("Command result: %s", out.String())
+	return out.String()
+}
+
+func RunBenchMark(wg *sync.WaitGroup, endpoint string, iterations int, outputFilePath string) {
+	defer wg.Done()
+	log.Infof("Running benchmark with %d iterations", iterations)
+	for i := 0; i < iterations; i++ {
+		log.Infof("Iteration %d", i)
+		command := `curl -X GET -H "x-api-key: <API_KEY>" -H "Content-Type: application/json" ` + endpoint
+		startTime := time.Now()
+		log.Infof("Sending request at %s", startTime)
+		RunCommandAndLog(exec.Command("sh", "-c", command))
+		endTime := time.Now()
+		log.Infof("Request completed at %s", endTime)
+		latency := endTime.Sub(startTime).Milliseconds()
+		log.Infof("Time taken for request: %d", latency)
+		WriteDataToFile(&[]string{strconv.FormatInt(latency, 10)}, outputFilePath)
+		time.Sleep(time.Duration(*burstIAT) * time.Millisecond)
+	}
+}
+
+func CreateFile(filePath string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Cannot create file %s", filePath)
+	}
+	defer file.Close()
+}
+
+func WriteDataToFile(data *[]string, outputFilePath string) {
+	log.Infof("Writing data to file %s", outputFilePath)
+	file, err := os.OpenFile(outputFilePath, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Fatalf("Cannot open file %s", outputFilePath)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	for _, value := range *data {
+		err := writer.Write([]string{value})
+		if err != nil {
+			log.Fatalf("Cannot write to file %s", outputFilePath)
+		}
 	}
 }
